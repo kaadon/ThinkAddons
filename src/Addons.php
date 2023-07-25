480a9713c485f4fbcb2801110869be4be1b3dfc8
kaadon.com <kaadon.com@gmail.com>
2023-07-25 09:31:06 +0800

    thinkphp6.*

--- src/Addons.php
+++ src/Addons.php
@@ -1,221 +1,189 @@
 <?php
-// +----------------------------------------------------------------------
-// | thinkphp5 Addons [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2016 http://www.zzstudio.net All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: Byron Sampson <xiaobo.sun@qq.com>
-// +----------------------------------------------------------------------
+/**
+ * +----------------------------------------------------------------------
+ * | think-addons [thinkphp6]
+ * +----------------------------------------------------------------------
+ *  .--,       .--,             | FILE: Addons.php
+ * ( (  \.---./  ) )            | AUTHOR: byron
+ *  '.__/o   o\__.'             | EMAIL: xiaobo.sun@qq.com
+ *     {=  ^  =}                | QQ: 150093589
+ *     /       \                | DATETIME: 2019/11/5 14:47
+ *    //       \\               |
+ *   //|   .   |\\              |
+ *   "'\       /'"_.-~^`'-.     |
+ *      \  _  /--'         `    |
+ *    ___)( )(___               |-----------------------------------------
+ *   (((__) (__)))              | 高山仰止,景行行止.虽不能至,心向往之。
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 2019 http://www.zzstudio.net All rights reserved.
+ * +----------------------------------------------------------------------
+ */
+declare(strict_types=1);
+
 namespace think;
 
-use think\facade\Env;
+use think\App;
+use think\helper\Str;
+use think\facade\Config;
 use think\facade\View;
-use think\Db;
 
-/**
- * 插件基类
- * Class Addons
- * @author Byron Sampson <xiaobo.sun@qq.com>
- * @package think\addons
- */
 abstract class Addons
 {
-    /**
-     * 视图实例对象
-     * @var view
-     * @access protected
-     */
-    protected $view = null;
-
-    // 当前错误信息
-    protected $error;
-
-    /**
-     * $info = [
-     *  'name'          => 'Test',
-     *  'title'         => '测试插件',
-     *  'description'   => '用于thinkphp5的插件扩展演示',
-     *  'status'        => 1,
-     *  'author'        => 'byron sampson',
-     *  'version'       => '0.1'
-     * ]
-     */
-    public $info = [];
-    public $addons_path = '';
-    public $config_file = '';
+    // app 容器
+    protected $app;
+    // 请求对象
+    protected $request;
+    // 当前插件标识
+    protected $name;
+    // 插件路径
+    protected $addon_path;
+    // 视图模型
+    protected $view;
+    // 插件配置
+    protected $addon_config;
+    // 插件信息
+    protected $addon_info;
 
     /**
-     * 架构函数
-     * @access public
+     * 插件构造函数
+     * Addons constructor.
+     * @param \think\App $app
      */
-    public function __construct()
+    public function __construct(App $app)
     {
-        // 获取当前插件目录
-        $this->addons_path = Env::get('addons_path') . $this->getName() . DIRECTORY_SEPARATOR;
-        // 读取当前插件配置信息
-        if (is_file($this->addons_path . 'config.php')) {
-            $this->config_file = $this->addons_path . 'config.php';
-        }
-
-        $config = ['view_path' => $this->addons_path];
-        $this->request = Container::get('request');
-        $this->app     = Container::get('app');
-        // 初始化视图模型
-        $this->view    = Container::get('view')->init(
-            array_merge($this->app['config']->get('template.'), $config)
-        );
+        $this->app = $app;
+        $this->request = $app->request;
+        $this->name = $this->getName();
+        $this->addon_path = $app->addons->getAddonsPath() . $this->name . DIRECTORY_SEPARATOR;
+        $this->addon_config = "addon_{$this->name}_config";
+        $this->addon_info = "addon_{$this->name}_info";
+        $this->view = clone View::engine('Think');
+        $this->view->config([
+            'view_path' => $this->addon_path . 'view' . DIRECTORY_SEPARATOR
+        ]);
 
         // 控制器初始化
-        if (method_exists($this, '_initialize')) {
-            $this->_initialize();
-        }
+        $this->initialize();
+    }
+
+    // 初始化
+    protected function initialize()
+    {}
+
+    /**
+     * 获取插件标识
+     * @return mixed|null
+     */
+    final protected function getName()
+    {
+        $class = get_class($this);
+        list(, $name, ) = explode('\\', $class);
+        $this->request->addon = $name;
+
+        return $name;
     }
 
     /**
-     * 获取插件信息
-     * @return array
+     * 加载模板输出
+     * @param string $template
+     * @param array $vars           模板文件名
+     * @return false|mixed|string   模板输出变量
+     * @throws \think\Exception
      */
-    final public function getInfo()
+    protected function fetch($template = '', $vars = [])
     {
-        return $this->info;
-    }
-
-    /**
-     * 获取插件的配置数组
-     * @param string $name 可选模块名
-     * @return array|mixed|null
-     */
-    final public function getConfig($name = '')
-    {
-        static $_config = array();
-        if (empty($name)) {
-            $name = $this->getName();
-        }
-        if (isset($_config[$name])) {
-            return $_config[$name];
-        }
-        $map['name'] = $name;
-        $map['status'] = 1;
-        $config = [];
-        if (is_file($this->config_file)) {
-            $temp_arr = include $this->config_file;
-            foreach ($temp_arr as $key => $value) {
-                if ($value['type'] == 'group') {
-                    foreach ($value['options'] as $gkey => $gvalue) {
-                        foreach ($gvalue['options'] as $ikey => $ivalue) {
-                            $config[$ikey] = $ivalue['value'];
-                        }
-                    }
-                } else {
-                    $config[$key] = $temp_arr[$key]['value'];
-                }
-            }
-            unset($temp_arr);
-        }
-        $_config[$name] = $config;
-
-        return $config;
-    }
-
-    /**
-     * 获取当前模块名
-     * @return string
-     */
-    final public function getName()
-    {
-        $data = explode('\\', get_class($this));
-        return strtolower(array_pop($data));
-    }
-
-    /**
-     * 检查配置信息是否完整
-     * @return bool
-     */
-    final public function checkInfo()
-    {
-        $info_check_keys = ['name', 'title', 'description', 'status', 'author', 'version'];
-        foreach ($info_check_keys as $value) {
-            if (!array_key_exists($value, $this->info)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 加载模板和页面输出 可以返回输出内容
-     * @access public
-     * @param string $template 模板文件名或者内容
-     * @param array $vars 模板输出变量
-     * @param array $replace 替换内容
-     * @param array $config 模板参数
-     * @return mixed
-     * @throws \Exception
-     */
-    public function fetch($template = '', $vars = [], $replace = [], $config = [])
-    {
-        if (!is_file($template)) {
-            $template = '/' . $template;
-        }
-        // 关闭模板布局
-        $this->view->engine->layout(false);
-
-        echo $this->view->fetch($template, $vars, $replace, $config);
+        return $this->view->fetch($template, $vars);
     }
 
     /**
      * 渲染内容输出
-     * @access public
-     * @param string $content 内容
-     * @param array $vars 模板输出变量
-     * @param array $replace 替换内容
-     * @param array $config 模板参数
+     * @access protected
+     * @param  string $content 模板内容
+     * @param  array  $vars    模板输出变量
      * @return mixed
      */
-    public function display($content, $vars = [], $replace = [], $config = [])
+    protected function display($content = '', $vars = [])
     {
-        // 关闭模板布局
-        $this->view->engine->layout(false);
-
-        echo $this->view->display($content, $vars, $replace, $config);
-    }
-
-    /**
-     * 渲染内容输出
-     * @access public
-     * @param string $content 内容
-     * @param array $vars 模板输出变量
-     * @return mixed
-     */
-    public function show($content, $vars = [])
-    {
-        // 关闭模板布局
-        $this->view->engine->layout(false);
-
-        echo $this->view->fetch($content, $vars, [], [], true);
+        return $this->view->display($content, $vars);
     }
 
     /**
      * 模板变量赋值
      * @access protected
-     * @param mixed $name 要显示的模板变量
-     * @param mixed $value 变量的值
-     * @return void
+     * @param  mixed $name  要显示的模板变量
+     * @param  mixed $value 变量的值
+     * @return $this
      */
-    public function assign($name, $value = '')
+    protected function assign($name, $value = '')
     {
-        $this->view->assign($name, $value);
+        $this->view->assign([$name => $value]);
+
+        return $this;
     }
 
     /**
-     * 获取当前错误信息
-     * @return mixed
+     * 初始化模板引擎
+     * @access protected
+     * @param  array|string $engine 引擎参数
+     * @return $this
      */
-    public function getError()
+    protected function engine($engine)
     {
-        return $this->error;
+        $this->view->engine($engine);
+
+        return $this;
+    }
+
+    /**
+     * 插件基础信息
+     * @return array
+     */
+    final public function getInfo()
+    {
+        $info = Config::get($this->addon_info, []);
+        if ($info) {
+            return $info;
+        }
+
+        // 文件属性
+        $info = $this->info ?? [];
+        // 文件配置
+        $info_file = $this->addon_path . 'info.ini';
+        if (is_file($info_file)) {
+            $_info = parse_ini_file($info_file, true, INI_SCANNER_TYPED) ?: [];
+            $_info['url'] = addons_url();
+            $info = array_merge($_info, $info);
+        }
+        Config::set($info, $this->addon_info);
+
+        return isset($info) ? $info : [];
+    }
+
+    /**
+     * 获取配置信息
+     * @param bool $type 是否获取完整配置
+     * @return array|mixed
+     */
+    final public function getConfig($type = false)
+    {
+        $config = Config::get($this->addon_config, []);
+        if ($config) {
+            return $config;
+        }
+        $config_file = $this->addon_path . 'config.php';
+        if (is_file($config_file)) {
+            $temp_arr = (array)include $config_file;
+            if ($type) {
+                return $temp_arr;
+            }
+            foreach ($temp_arr as $key => $value) {
+                $config[$key] = $value['value'];
+            }
+            unset($temp_arr);
+        }
+        Config::set($config, $this->addon_config);
+
+        return $config;
     }
 
     //必须实现安装
